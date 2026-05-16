131389d8d4c6572fc6912c7bf596516e0347c8ad
kdw023765 <kdw023765@kyonggi.ac.kr>
2026-05-17 00:20:44 +0900

    Update nagging_request.dart

--- nagging_request.dart
+++ nagging_request.dart
@@ -1,1 +1,19 @@
+class NaggingRequest {
+  final String uid;
+  final String zoneName;
+  final String zoneCategory;
+  final int todayBudget;
+  final int remainingBudget;
+  final double budgetUsageRate;
+  final DateTime enteredAt;
 
+  NaggingRequest({
+    required this.uid,
+    required this.zoneName,
+    required this.zoneCategory,
+    required this.todayBudget,
+    required this.remainingBudget,
+    required this.budgetUsageRate,
+    required this.enteredAt,
+  });
+}
