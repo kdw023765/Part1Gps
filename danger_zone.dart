bf070322debc29058a74f417b3cb7da5614b3f0f
kdw023765 <kdw023765@kyonggi.ac.kr>
2026-05-17 00:21:42 +0900

    Update danger_zone.dart

--- danger_zone.dart
+++ danger_zone.dart
@@ -1,1 +1,25 @@
+class DangerZone {
+  final String docId;
+  final String zoneName;
+  final String zoneCategory; // app_constants.dart 의 DangerZoneCategories 사용
+  final double latitude;
+  final double longitude;
 
+  DangerZone({
+    required this.docId,
+    required this.zoneName,
+    required this.zoneCategory,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'docId': docId,
+      'zoneName': zoneName,
+      'zoneCategory': zoneCategory,
+      'latitude': latitude,
+      'longitude': longitude,
+    };
+  }
+}
